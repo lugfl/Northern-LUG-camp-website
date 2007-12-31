853cda891e264cd55437409b2bd819ce4d57c075
balumir <balumir@631c280c-4842-0410-8b2f-e4ed0ad51857>
2007-12-31 12:18:07 +0000

    Schreibfehler korrigiert
    git-svn-id: http://svndemo.fagerholm.de/lc2008/trunk@3 631c280c-4842-0410-8b2f-e4ed0ad51857

--- web/pages/page.anreise_schiff.php
+++ web/pages/page.anreise_schiff.php
@@ -12,7 +12,7 @@ class HtmlPage_anreise_schiff extends HtmlPage {
     		$ret = '
 <h1>Schiff</h1>
 
-<p>Wenn ihr mit dem Schiff anreist, dann m&uuml;&szlig;t ihr euch erstmal durch die dichte Phalanx der Tradition&szlig;chiffe durchw&uuml;hlen, die im Rahmen der <a href="http://www.rumregatta.de" target="_blank">Rum-Regatta</a> die Flensburger F&ouml;rde unsicher machen. Wenn ihr das geschafft habt, dann sucht ihr euch im Innenhafen einen sch&ouml;nen Liegeplatz und ruft uns &uuml;ber das Orga-Telefon (0171 - 233 85 96) an, damit wir euch dort abholen und zum LUG Camp bringen k&ouml;nnen.
+<p>Wenn ihr mit dem Schiff anreist, dann m&uuml;&szlig;t ihr euch erstmal durch die dichte Phalanx der Traditionsschiffe durchw&uuml;hlen, die im Rahmen der <a href="http://www.rumregatta.de" target="_blank">Rum-Regatta</a> die Flensburger F&ouml;rde unsicher machen. Wenn ihr das geschafft habt, dann sucht ihr euch im Innenhafen einen sch&ouml;nen Liegeplatz und ruft uns &uuml;ber das Orga-Telefon (0171 - 233 85 96) an, damit wir euch dort abholen und zum LUG Camp bringen k&ouml;nnen.
 
 		';
 		return $ret;
