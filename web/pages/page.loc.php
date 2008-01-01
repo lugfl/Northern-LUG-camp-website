43042d7bf39145b63fcc824f99262863dd5100f7
balumir <balumir@631c280c-4842-0410-8b2f-e4ed0ad51857>
2008-01-01 09:38:19 +0000

    Verlinkung Sportland im Text auf der ersten location Seite
    git-svn-id: http://svndemo.fagerholm.de/lc2008/trunk@24 631c280c-4842-0410-8b2f-e4ed0ad51857

--- web/pages/page.loc.php
+++ web/pages/page.loc.php
@@ -12,7 +12,7 @@ class HtmlPage_loc extends HtmlPage {
     		$ret = '
 <h1>Location</h1>
 <p><img src="images/mike/flensburg-innenhafen-200px.jpg" align="left" style="margin-right:10px;"/>Ohne Location geht es nicht.<br/><br/>
-Das LUG-Camp 2008 findet dieses Jahr in Flensburg an der Ostsee im sch&ouml;nsten Bundesland der Welt statt, n&auml;mlich <a href="http://www.schleswig-holstein.de" target="_blank">Schleswig-Holstein</a>.<br>
+Das LUG-Camp 2008 findet dieses Jahr im <a href="?p=loc_sportland">Sportland</a> in Flensburg an der Ostsee im sch&ouml;nsten Bundesland der Welt statt, n&auml;mlich <a href="http://www.schleswig-holstein.de" target="_blank">Schleswig-Holstein</a>.<br>
 </p>
 
 <p>Ihr wollt mehr Informationen. dann klickt euch mal <a href="http://www.flensburg.de" target="_blank">durch</a>. </p>
