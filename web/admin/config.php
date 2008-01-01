d6af4dea9180e877fe9c810a7ab6d09805681efc
balumir <balumir@631c280c-4842-0410-8b2f-e4ed0ad51857>
2008-01-01 16:24:07 +0000

    Configdatei .htconfig.php optinal gemacht und aus dem subversion entfernt, damit keine Zugangskonfigs im subversion liegen
    git-svn-id: http://svndemo.fagerholm.de/lc2008/trunk@32 631c280c-4842-0410-8b2f-e4ed0ad51857

--- web/admin/config.php
+++ web/admin/config.php
@@ -1,10 +1,44 @@
 <?php
 
-include_once('.htconfig.php');
+// Default DB
+$DB_user = 'test';
+$DB_pass = 'test';
+$DB_name = 'test';
+$DB_host = "localhost";
+
+
+$DB['DEFAULT']['user'] = $DB_user;
+$DB['DEFAULT']['pass'] = $DB_pass;
+$DB['DEFAULT']['name'] = $DB_name;
+$DB['DEFAULT']['host'] = $DB_host;
 
 define ('WEB_DATEFORMAT','%d.%m.%Y, %H:%m');
 define ('WEB_NEWSPAGE','./news.php');
 define ('WEB_NEWSTEASER_ANZAHL',3);
 define ('DEBUG',0);
 
+$DB_SCHEMA['news_cat']['name'] = 'News Kategorie';
+$DB_SCHEMA['news_cat']['cols']['catid']['name'] = '';
+$DB_SCHEMA['news_cat']['cols']['name']['name'] = 'Kategorie';
+$DB_SCHEMA['news_cat']['cols']['pic']['name'] = 'Bild';
+
+$DB_SCHEMA['news_eintrag']['name'] = 'News';
+
+$DB_SCHEMA['news_eintrag']['cols']['eintragid']['name'] = '';
+$DB_SCHEMA['news_eintrag']['cols']['eintragid']['cmd']['detailview']['name'] = 'Details';
+$DB_SCHEMA['news_eintrag']['cols']['eintragid']['cmd']['detailview']['p'] = 'news';
+$DB_SCHEMA['news_eintrag']['cols']['eintragid']['cmd']['edit']['name'] = 'Editieren';
+$DB_SCHEMA['news_eintrag']['cols']['eintragid']['cmd']['edit']['p'] = 'news';
+$DB_SCHEMA['news_eintrag']['cols']['eintragid']['cmd']['delete']['name'] = 'L&ouml;schen';
+$DB_SCHEMA['news_eintrag']['cols']['eintragid']['cmd']['delete']['p'] = 'news';
+
+$DB_SCHEMA['news_eintrag']['cols']['title']['name'] = 'Titel';
+$DB_SCHEMA['news_eintrag']['cols']['short']['name'] = 'Teaser';
+$DB_SCHEMA['news_eintrag']['cols']['txt']['name'] = 'Newstext';
+$DB_SCHEMA['news_eintrag']['cols']['crdate']['name'] = 'Erstellt am';
+$DB_SCHEMA['news_eintrag']['cols']['author']['name'] = 'Autor';
+
+if( is_file('.htconfig.php') ) {
+	include_once('.htconfig.php');
+}
 ?>
