526c7f5e9ea7053cbb7acfd14243162e28ac19ff
balumir <balumir@631c280c-4842-0410-8b2f-e4ed0ad51857>
2008-01-04 19:44:51 +0000

    Verlinkung zur Mailingliste eingebaut Mantis-ID: 0000026
    git-svn-id: http://svndemo.fagerholm.de/lc2008/trunk@41 631c280c-4842-0410-8b2f-e4ed0ad51857

--- web/pages/page.lugcamp.php
+++ web/pages/page.lugcamp.php
@@ -12,11 +12,21 @@ class HtmlPage_lugcamp extends HtmlPage {
     		$ret = '
 <h1>Das LUG Camp</h1>
 
+<p>
 Das LUG Camp ist ein Treffen von Linux User Groups und Linux-Freunden aus dem gesamten deutschsprachigen Raum. Die &quot;Erfinder&quot; sind die <a href="http://www.lugal.de" target="_blank">LUG Allg&auml;u</a> und die <a href="http://www.luga.de" target="_blank">LUG Augsburg</a>, die seither nat&uuml;rlich auch bei jedem LUG Camp dabei waren.
+</p>
 
+<p>
 Genaueres erfahrt ihr - war ja klar - in der <a href="http://de.wikipedia.org/wiki/LUG-Camp" target="blank">Wikipedia</a> und im <a href="http://www.lug-camp-howto.de" target="blank">LUG Camp-Howto</a>, einer Zusammenstellung vieler Hinweise, Tips und Erfahrungen der Ausrichter und der Teilnehmer der bisherigen LUG Camps.
+</p>
 
+<p>Wenn ihr immer auf dem Laufenden bleiben wollt, was das aktuelle Camp und auch die Folgenden angeht, meldet euch doch einfach an der <a href="http://lists.lugcamp.org/cgi-bin/mailman/listinfo/teilnehmer">Camp Mailingliste</a> an. Neu ist ab diesem Jahr, das diese Liste nicht nur f&uuml;r 2008 gilt, sondern f&uuml;r die Folgejahre aktiv sein wird.
+</p>
+
+<p>
 Auf jeden Fall ist das Camp ein Ereignis, dass Du auf keinen Fall verpassen solltest!
+</p>
+
 
 		';
 		return $ret;
