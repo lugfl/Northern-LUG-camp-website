590e73ecf48facbffe29038b609fb068a7a0575a
balumir <balumir@631c280c-4842-0410-8b2f-e4ed0ad51857>
2008-01-21 20:23:02 +0000

    Newssystemausgaben auf DIVs umgebaut
    git-svn-id: http://svndemo.fagerholm.de/lc2008/trunk@80 631c280c-4842-0410-8b2f-e4ed0ad51857

--- web/pages/page.news.php
+++ web/pages/page.news.php
@@ -30,84 +30,76 @@ class HtmlPage_news extends HtmlPage {
 			$SQL .= " LIMIT ". WEB_NEWSTEASER_ANZAHL;
 		}
 		$news_query = my_query($SQL);
-		if($news_query) {
-			while($news_row = mysql_fetch_object($news_query))
-			{
+		if($news_query && mysql_num_rows($news_query)>0) {
+			$content .= '
+				<div class="news-list">
+			';
+			while($news_row = mysql_fetch_object($news_query)) {   
 				$content .= '
-				<table id="news">
-					<tr>
-						<th>'.$news_row->title.'</th>
-					</tr>
-					<tr>
-						<td class="cat">';
-				$catpic_file = "pics/cat_news/".$news_row->catpic;
-				if( is_file($catpic_file) ) {
-					$content .=  '<img src="pics/cat_news/'.$news_row->catpic.'" alt="'.$news_row->catname.'">';
-				}else{
-					$content .=  "Kategorie: ".$news_row->catname;
-				}
-				$content .=  '</td>
-					</tr>
-					<tr>
-						<td>'.nl2br($news_row->short).'</td>
-					</tr>
-					<tr>
-						<td class="author">News geschrieben am '.$news_row->crdatestr.' von '.$news_row->author.'</td>
-					</tr>
-					<tr>
-						<td><a href="index.php?p=news&news='.$news_row->eintragid.'">mehr lesen</a></td>
-					</tr>
-					<tr>
-						<td>&nbsp;</td>
-					</tr>
-				</table>';
+					<div class="news-single">
+					<div class="news-title">'.$news_row->title.'</div>
+				';
+				if($news_row->catname != '') {
+					$content .= '
+						<div class="news-cat">
+					';
+					if($news_row->catpic!='') {
+						$catpic_file = "pics/cat_news/".$news_row->catpic;
+						if( is_file($catpic_file) ) { 
+							$content .=  '<img src="pics/cat_news/'.$news_row->catpic.'" alt="'.$news_row->catname.'" class="news-catpic">';
+						}else{
+							$content .=  "Kategorie: ".$news_row->catname;
+						}
+					}
+					$content .= '
+						</div>
+					';
+				} // if catname
+				if($news_row->short != '') {
+					$content .= '
+						<div class="news-teaser">'.nl2br($news_row->short).'</div>
+					';
+				}   
+				$content .= '
+					<div class="news-author">News geschrieben am '.$news_row->crdatestr.' von '.$news_row->author.'</div>
+					<div class="news-more"><a href="?p=news&amp;newsid='.$news_row->eintragid.'">mehr lesen</a></div>
+				</div>';
 			} // while
+			$content .= '
+			</div>';
 			mysql_free_result($news_query);
-			if($limit != 'no')
-			{
-				$content .=  '<p><a href="?p=news&limit=no">Alle News anzeigen</a></p>';
-			}
-			else
-			{ // if news_query
-				$content .=  "<!-- MySQL-Fehler: ".mysql_errno() . " -->";
-				$content .=  "Momentan sind keine News eingetragen.";
-			}
+			if($limit != 'no') {   
+				$content .=  '<p><a href="?p=news&amp;limit=no" class="news-navi-link">Alle News anzeigen</a></p>';
+			}   
+		} else { // if news_query
+			$content .=  "<!-- MySQL-Fehler: ".mysql_errno() . " -->";
+			$content .=  "Momentan sind keine News eingetragen.";
 		}
 		return $content;
 	}
-	
 	
 	function news_lesen($news_id)
 	{
 		$content = '';
 		$SQL = "SELECT e.eintragid,e.title,e.catid,e.short,e.txt,DATE_FORMAT(crdate,'".WEB_DATEFORMAT."') crdatestr,e.author,c.pic AS catpic,c.name AS catname FROM news_eintrag e LEFT JOIN news_cat c ON e.catid=c.catid WHERE e.eintragid='$news_id'";
 		$news_query = my_query($SQL);
-		if($news_query)	
-		{
+
+		if($news_query) {
 			$news_daten = mysql_fetch_array($news_query);
-			$content .= '<table id="news">
-				<tr>
-					<th>'.$news_daten["title"].'</th>
-				</tr>
-				<tr>
-					<td class="cat">';
+			$content .= '
+				<div class="news-single"
+				<div class="news-title">'.$news_daten["title"].'</div>
+				<div class="news-cat">';
 			if( is_file($news_daten['catpic']) ) {
 				$content .=  '<img src="pics/cat_news/'.$news_daten['catpic'].'" alt="'.$news_daten['catname'].'">';
-			}
-			else
-			{
+			}else{
 				$content .=  "Kategorie: ".$news_daten['catname'];
 			}
-			$content .= '</td>
-				</tr>
-				<tr>
-					<td>'.nl2br($news_daten["txt"]).'</td>
-				</tr>
-				<tr>
-					<td class="author">News geschrieben am '.$news_daten["crdatestr"].' von '.$news_daten["author"].'</td>
-				</tr>
-			</table>';
-			$content .= '<a href="./index.php?p=news">zur&uuml;ck zur News&uuml;bersicht.</a>';
+			$content .= '</div>
+				<div class="news-text">'.nl2br($news_daten["txt"]).'</div>
+				<div class="news-author">News geschrieben am '.$news_daten["crdatestr"].' von '.$news_daten["author"].'</div>
+			</div>';
+			$content .= '<a href="?p=news" class="news-navi-link">zur&uuml;ck zur News&uuml;bersicht.</a>';
 			mysql_free_result($news_query);
 		}else{ // if news_query
 			$content .=  "<!-- MySQL-Fehler: ".mysql_errno() . " -->";
@@ -118,10 +110,10 @@ class HtmlPage_news extends HtmlPage {
 
 
 	function getContent() {
-		$news_id = http_get_var("news");
-		if($news_id)
+		$newsid = http_get_var("newsid");
+		if($newsid)
 		{
-			$ret = $this->news_lesen($news_id);
+			$ret = $this->news_lesen($newsid);
 		}
 		else
 		{
