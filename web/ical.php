82cd0115416aedd46b3ba32665ae5ad5f453b573
balumir <balumir@631c280c-4842-0410-8b2f-e4ed0ad51857>
2008-02-18 22:14:04 +0000

    Branch release-080218 angelegt
    git-svn-id: http://svndemo.fagerholm.de/lc2008/trunk@137 631c280c-4842-0410-8b2f-e4ed0ad51857

--- web/ical.php
+++ web/ical.php
@@ -2,9 +2,29 @@
 
 require_once('lib/func.parse_ical.php');
 
-print '<pre>';
-print_r(parse_ical('lc2008-all'));
-print '</pre>';
+function parse_ical_utf8($name) {
+	$filename = $name . ".ics";
 
+	if($fp = fopen($filename,"r")) {
+		$in = fread($fp,filesize($filename));
+		fclose($fp);
+
+		$str = iconv('utf-8','ISO-8859-1',$in);
+		if($fp = fopen('/tmp/'.$name.'-ISO.ics',"w")) {
+			fputs($fp,$str);
+			fclose($fp);
+
+			print_r(parse_ical('/tmp/lc2008-all-ISO'));
+		}
+	}
+}
+
+parse_ical_utf8('lc2008-all');
+
+/*
+print '<pre>';
+print_r(parse_ical('lc2008-all-ISO'));
+print '</pre>';
+*/
 
 ?>
