50d11fcc5b2f375e0d7d103ae51b9b932bc75f50
Stefan <Stefan@631c280c-4842-0410-8b2f-e4ed0ad51857>
2008-02-18 21:36:13 +0000

    noch zu zahlender Betrag wird in der Anmeldungsliste angezeigt !
    git-svn-id: http://svndemo.fagerholm.de/lc2008/trunk@135 631c280c-4842-0410-8b2f-e4ed0ad51857

--- web/admin/pages/page.anmeldungsliste.php
+++ web/admin/pages/page.anmeldungsliste.php
@@ -20,7 +20,7 @@ class HtmlPage_anmeldungsliste extends HtmlPage {
 	function getContent() {
     		$ret = '';
 		$SQL = "SELECT a.accountid,a.username,a.acl,UNIX_TIMESTAMP(a.crdate) AS crdate,a.active, l.name AS lugname, ";
-		$SQL .= " an.vorname,an.nachname ";
+		$SQL .= " an.anmeldungid,an.vorname,an.nachname ";
 		$SQL .= " FROM account a LEFT JOIN event_lug l ON a.lugid=l.lugid ";
 		$SQL .= " LEFT JOIN event_anmeldung an ON a.accountid=an.accountid ";
 		$SQL .= " ORDER BY crdate ";
@@ -38,10 +38,47 @@ class HtmlPage_anmeldungsliste extends HtmlPage {
 						<th>LUG</th>
 						<th>Anmeldedatum</th>
 						<th>Accountstatus</th>
+						<th>Bezahlstatus</th>
 					</tr>
 				';
 				$ctr = 1;
+
+				$events_SQL = "SELECT eventid,charge FROM event_event";
+				$events_res = my_query($events_SQL);
+				$events_array = array();
+				while($events_row = mysql_fetch_assoc($events_res)) {
+					$events_array[$events_row['eventid']] = $events_row['charge'];
+				}
+
+				$artikel_SQL = "SELECT artikelid,preis FROM event_artikel";
+				$artikel_res = my_query($artikel_SQL);
+				$artikel_array = array();
+				while($artikel_row = mysql_fetch_assoc($artikel_res)) {
+					$artikel_array[$artikel_row['artikelid']] = $artikel_row['preis'];
+				}
+
 				while($row = mysql_fetch_assoc($res)) {
+					$topay = 0;
+					$payed = 0;
+					
+					$e_SQL = "SELECT eventid,bezahlt FROM event_anmeldung_event WHERE anmeldungid = '".$row['anmeldungid']."'";
+					$e_res = my_query($e_SQL);
+					while($e_row = mysql_fetch_assoc($e_res)) {
+						$topay += $events_array[$e_row['eventid']];
+						if($e_row['bezahlt'] != 0) {
+							$payed += $events_array[$e_row['eventid']];
+						}
+					}
+					
+					$a_SQL = "SELECT artikelid,bezahlt FROM event_account_artikel WHERE accountid = '".$row['accountid']."'";
+					$a_res = my_query($a_SQL);
+					while($a_row = mysql_fetch_assoc($a_res)) {
+						$topay += $artikel_array[$a_row['artikelid']];
+						if($a_row['bezahlt'] != 0) {
+							$payed += $artikel_array[$a_row['artikelid']];
+						}
+					}
+					
 					$crdate = date("d.m.Y G:i:s",$row['crdate']);
 					$act = ($row['active'] ? 'aktiv': '- <a href="?p=remail&a='.$row['accountid'].'">Aktivierungsmail senden</a>');
 					$ret .= '
@@ -53,6 +90,7 @@ class HtmlPage_anmeldungsliste extends HtmlPage {
 						<td>'.$row['lugname'].'</td>
 						<td>'.$crdate.'</td>
 						<td>'.$act.'</td>
+						<td>'.($topay-$payed).'&euro; zu zahlen</td>
 					</tr>
 					';
 					$ctr++;
