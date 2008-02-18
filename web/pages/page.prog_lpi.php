9dce7cc7ba7d612c6bcc35ceb56fe29b1b25e6e0
balumir <balumir@631c280c-4842-0410-8b2f-e4ed0ad51857>
2008-02-18 21:18:03 +0000

    LPI-Pruefungsliste eingebaut.
    git-svn-id: http://svndemo.fagerholm.de/lc2008/trunk@132 631c280c-4842-0410-8b2f-e4ed0ad51857

--- web/pages/page.prog_lpi.php
+++ web/pages/page.prog_lpi.php
@@ -11,7 +11,22 @@ class HtmlPage_prog_lpi extends HtmlPage {
 	function getContent() {
     		$ret = '
 		<h1>LPI-Pr&uuml;fungen</h1>
-		<p>TODO</p>
+		<p>Die folgenden Pr&uuml;fungen werden jeweils um 09:00 Uhr und um 11:00 Uhr am Sa. 03.05. angeboten:</p>
+		<img src="./images/lpi-tux-3-lowres.jpg" align="right"  style="margin-right:10px;"/>
+		<ul>
+			<li>LPI-101 (deutsch oder englisch), 70 &euro;</li>
+			<li>LPI-102 (deutsch oder englisch), 70 &euro;</li>
+			<li>LPI-201 (deutsch oder englisch), 70 &euro;</li>
+			<li>LPI-202 (deutsch oder englisch), 70 &euro;</li>
+			<li>LPI-301 (nur englisch), 90 &euro;</li>
+			<li>LPI-302 (nur englisch), 70 &euro;</li>
+			<li>Ubuntu-LPI-199 (nur englisch), 85 &euro; (f&uuml;r UBUNTU Member 75 &euro;)</li>
+		</ul>
+
+		<p>
+			F&uuml;r die beiden Pr&uuml;fungstermine gelten die Pr&uuml;fungsbedingungen des LPI. Jeder Teilnehmer muss sich vor dem Event unter <a href="http://lpievent.lpi-german.de/" target="_blank">lpievent.lpi-german.de</a> anmelden. 
+		</p>
+
 		';
 		return $ret;
 	}
