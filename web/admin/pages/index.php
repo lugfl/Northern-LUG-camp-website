ca11fab1e60db1693cf1b15cad50950521c78741
Stefan <Stefan@631c280c-4842-0410-8b2f-e4ed0ad51857>
2008-03-25 22:55:17 +0000

    T-Shirt anzeige im Adminpanel eingebaut
    git-svn-id: http://svndemo.fagerholm.de/lc2008/trunk@147 631c280c-4842-0410-8b2f-e4ed0ad51857

--- web/admin/pages/index.php
+++ web/admin/pages/index.php
@@ -9,14 +9,15 @@ require_once('lib/class.HtmlPageNavi.php');
 
 require_once('pages/page.start.php');
 require_once('pages/page.news.php');
-require_once('pages/page.kasse.php');
-require_once('pages/page.remail.php');
 require_once('pages/page.anmeldungsliste.php');
 require_once('pages/page.eventanmeldungen.php');
-require_once('pages/page.account.php');
+require_once('pages/page.kasse.php');
 require_once('pages/page.lugs.php');
+require_once('pages/page.shirts.php');
 require_once('pages/page.vegetarier.php');
 
+require_once('pages/page.remail.php');
+require_once('pages/page.account.php');
 /*
 require_once('pages/page.demo1_1.php');
 require_once('pages/page.demo1_2a.php');
---
+++ web/admin/pages/page.shirts.php
@@ -0,0 +1,61 @@
+<?php
+
+$PAGE['shirts']['name'] = "T-Shirts";
+$PAGE['shirts']['navilevel'] = 1;
+$PAGE['shirts']['login_required'] = 1;
+$PAGE['shirts']['phpclass'] = 'HtmlPage_shirts';
+$PAGE['shirts']['parent'] = 'root';
+
+require_once('lib/inc.database.php');
+
+class HtmlPage_shirts extends HtmlPage {
+
+	var $name = "T-Shirts";
+	var $navilevel = 1;
+	var $login_required = 1;
+
+	function HtmlPage_shirts() {
+	}
+	
+	function getContent() {
+    		$ret = '';
+		$SQL = "SELECT * FROM event_artikel";
+		$res = my_query($SQL);
+		$ret .= '
+		<table>
+			<tr>
+				<th>Artikel</th>
+				<th>Gr&ouml&szlig;e</th>
+				<th>Anzahl</th>
+			</tr>
+		';
+		
+		while($row = mysql_fetch_object($res))
+		{
+			$anzahl = 0;
+			$groesse = '';
+			$eintrag = '';
+			$SQL = "SELECT * FROM event_account_artikel WHERE artikelid = '".$row->artikelid."' ORDER BY groesse";
+			$res = my_query($SQL);
+			while($brow = mysql_fetch_object($res)) {
+				if($groesse != $brow->groesse && $groesse != '') {
+					$ret .= '<tr><td>'.$row->name.'</td><td>'.$groesse.'</td><td>'.$anzahl.'</td></tr>';
+					$anzahl = $brow->anzahl;
+				} else {
+					$anzahl += $brow->anzahl;		
+				}
+				$groesse = $brow->groesse;
+			}
+			$ret .= '<tr><td>'.$row->name.'</td><td>'.$groesse.'</td><td>'.$anzahl.'</td></tr>';
+		}
+
+		$ret .= '
+		</table>
+		';
+		mysql_free_result($res);
+		return $ret;
+	}
+}
+
+
+?>
