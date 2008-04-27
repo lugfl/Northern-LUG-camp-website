48e107f2ef8169bf04592e3cdcdeaed2007f62f8
Stefan <Stefan@631c280c-4842-0410-8b2f-e4ed0ad51857>
2008-04-27 15:13:02 +0000

    CSV Export eingebaut
    git-svn-id: http://svndemo.fagerholm.de/lc2008/trunk@175 631c280c-4842-0410-8b2f-e4ed0ad51857

--- web/admin/pages/page.anmeldungsliste.csv.php
+++ web/admin/pages/page.anmeldungsliste.csv.php
@@ -18,25 +18,53 @@ class HtmlPage_anmeldungsliste_csv extends HtmlPage {
 	}
 	
 	function paystatus($anmeldungid,$accountid,$events_array,$artikel_array) {
-		$array = array();
+		$array = array('brauerei'=>"nicht angemeldet","abzeichen"=>"");
 		$e_SQL = "SELECT eventid,bezahlt FROM event_anmeldung_event WHERE anmeldungid = '".$anmeldungid."'";
 		$e_res = my_query($e_SQL);
 		while($e_row = mysql_fetch_assoc($e_res)) {
+			if($e_row['eventid'] == 1) {
+				$array['brauerei'] = "NICHT bezahlt";
+			}
+			if($e_row['eventid'] == 3) {
+				$array['camp'] = "Nein";
+			}
+			if($e_row['eventid'] == 4) {
+				$array['urkunde'] = "NICHT bezahlt";
+			}
+			if($e_row['eventid'] == 5 || $e_row['eventid'] == 6 || $e_row['eventid'] == 7 || $e_row['eventid'] == 8) {
+				$array['abzeichen'] .= $events_array[$e_row['eventid']]['name'].", ";
+			}
 			$array['events'][$e_row['eventid']] = $e_row['bezahlt'];
-			$topay += $events_array[$e_row['eventid']];
+			$topay += $events_array[$e_row['eventid']]['charge'];
 			if($e_row['bezahlt'] != 0) {
-				$payed += $events_array[$e_row['eventid']];
+				if($e_row['eventid'] == 1) {
+					$array['brauerei'] = "bezahlt";
+				}
+				if($e_row['eventid'] == 3) {
+					$array['camp'] = "Ja";
+				}
+				if($e_row['eventid'] == 4) {
+					$array['urkunde'] = "bezahlt";
+				}
+				$payed += $events_array[$e_row['eventid']]['charge'];
 			}
 		}
+		
+		if($array['urkunde']) { $array['abzeichen'] .= ' ('.$array['urkunde'].')'; }
 						
-		$a_SQL = "SELECT artikelid,anzahl,bezahlt FROM event_account_artikel WHERE accountid = '".$accountid."'";
+		$a_SQL = "SELECT artikelid,anzahl,bezahlt,groesse FROM event_account_artikel WHERE accountid = '".$accountid."'";
 		$a_res = my_query($a_SQL);
 		while($a_row = mysql_fetch_assoc($a_res)) {
+			$array['shirts'] .= $a_row['anzahl']."x ".$artikel_array[$a_row['artikelid']]['name']." ".$a_row['groesse'];
 			$array['artikel'][$a_row['artikelid']] = $a_row['bezahlt'];
-			$topay += $a_row['anzahl']*$artikel_array[$a_row['artikelid']];
+			$topay += $a_row['anzahl']*$artikel_array[$a_row['artikelid']]['charge'];
 			if($a_row['bezahlt'] != 0) {
-				$payed +=  $a_row['anzahl']*$artikel_array[$a_row['artikelid']];
+				$payed +=  $a_row['anzahl']*$artikel_array[$a_row['artikelid']]['charge'];
+				$array['shirts'] .= ' (bezahlt)';
+			} else {
+				$array['shirts'] .= ' (NICHT bezahlt)';
 			}
+			$array['shirts'] .= ", ";
 		}
 		
 		$array['topay'] = $topay;
@@ -56,38 +84,41 @@ class HtmlPage_anmeldungsliste_csv extends HtmlPage {
 		
 		if($res) {
 			if(mysql_num_rows($res) >0) {
-				$ret .= '"Nr";"Vorname";"Name";"Nickname";"LUG";"Beitrag";"T-Shirts";"Brauerei";"Schwimmabzeichen"'."\n";
+				$csv = '"Nr";"Vorname";"Name";"Nickname";"LUG";"Beitrag";"T-Shirts";"Brauerei";"Schwimmabzeichen"'."\n";
 				$ctr = 1;
 
-				$events_SQL = "SELECT eventid,charge FROM event_event";
+				$events_SQL = "SELECT eventid,name,charge FROM event_event";
 				$events_res = my_query($events_SQL);
 				$events_array = array();
 				while($events_row = mysql_fetch_assoc($events_res)) {
-					$events_array[$events_row['eventid']] = $events_row['charge'];
+					$events_array[$events_row['eventid']]['charge'] = $events_row['charge'];
+					$events_array[$events_row['eventid']]['name'] = $events_row['name'];
 				}
 
-				$artikel_SQL = "SELECT artikelid,preis FROM event_artikel";
+				$artikel_SQL = "SELECT artikelid,name,preis FROM event_artikel";
 				$artikel_res = my_query($artikel_SQL);
 				$artikel_array = array();
 				while($artikel_row = mysql_fetch_assoc($artikel_res)) {
-					$artikel_array[$artikel_row['artikelid']] = $artikel_row['preis'];
+					$artikel_array[$artikel_row['artikelid']]['charge'] = $artikel_row['preis'];
+					$artikel_array[$artikel_row['artikelid']]['name'] = $artikel_row['name'];
 				}
-				
 			
 				while($row = mysql_fetch_assoc($res)) {
 					$pay_array = $this->paystatus($row['anmeldungid'],$row['accountid'],$events_array,$artikel_array);
-						
+					$paystatus = $pay_array['camp'];
 					$crdate = date("d.m.Y G:i:s",$row['crdate']);
-					$act = ($row['active'] ? 'aktiv': '- <a href="?p=remail&a='.$row['accountid'].'">Aktivierungsmail senden</a>');
-					$ret .= $ctr.';"'.$row['vorname'].'";"'.$row['nachname'].'";"'.$row['username'].'";"'.$row['lugname'].'";"'.$paystatus.'"';
-					$ret .= '"'..'";"'..'";"'..'"';
-					$ret .= "\n";
+					$csv .= $ctr.';"'.$row['vorname'].'";"'.$row['nachname'].'";"'.$row['username'].'";"'.$row['lugname'].'";"'.$paystatus.'";';
+					$csv .= '"'.$pay_array['shirts'].'";"'.$pay_array['brauerei'].'";"'.$pay_array['abzeichen'].'"';
+					$csv .= "\n";
 					$ctr++;
 				}
 			}
 			mysql_free_result($res);
 		}
-		
+		$handle = fopen("tmp/anmeldungen.csv","w");
+		fwrite($handle, $csv);
+		fclose($handle);
+		$ret .= '<a href="tmp/anmeldungen.csv">klick</a>';
 		return $ret;
 	}
 
