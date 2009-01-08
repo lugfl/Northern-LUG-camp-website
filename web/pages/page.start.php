b918ed6cbb18684b24db171518149108bd4514d6
balumir <balumir@631c280c-4842-0410-8b2f-e4ed0ad51857>
2009-01-08 20:20:50 +0000

    Hinweistext auf LC2009 auf der Startseite eingebaut.
    git-svn-id: http://svndemo.fagerholm.de/lc2008/trunk@181 631c280c-4842-0410-8b2f-e4ed0ad51857

--- web/pages/page.start.php
+++ web/pages/page.start.php
@@ -21,8 +21,11 @@ Und wenn wir etwas <b>aus</b>richten, dann richtet ihr es doch einfach <b>ein</b
 <h2>1. bis 4. Mai 2008</h2>
 
 im <a href="http://www.sportland-flensburg.de">Sportland Flensburg</a> an diesem Event teilnehmen werdet und dass ihr der &Uuml;bergabe des "LUG Camp-Cup" beiwohnt!<br/><br/>
-<img src="bilder/camp-cup.jpg" alt="LUG-CAMP Cup" style="margin-left:300px">
+<img src="bilder/camp-cup.jpg" alt="LUG-CAMP Cup" style="margin-left:0px" align="left" hspace="10" vspace="10">
 
+<h1>NEWS: Termin und Lokation LUG Camp 2009</h1>
+
+Die Veranstalter des LUG-Camps 2009 haben die Bombe platzen lassen. Das Camp findet vom 21.-24.05.2009 im <a href="http://www.kleiter.de/Seiten/Unsere_Haeuser/Gschwender-Hof.php" target="_blank">Gschwender Hof</a> statt. Weitere Details findet Ihr in ein paar Tagen auf der Webseite <a href="http://www.lug-camp.de">lug-camp.de</a> sobald die Webseite online gegangen ist.
 		';
 		return $ret;
 	}
