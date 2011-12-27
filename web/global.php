81b8b4244248a8aad164ef43fccd7f87dc817bec
Frank Agerholm <franka@serverwg.de>
2011-12-27 15:10:28 +0100

    Added some Const definitions fors Smarty engine and styles-system

--- web/global.php
+++ web/global.php
@@ -3,6 +3,15 @@
 setlocale (LC_ALL, 'de_DE');
 
 define('WEB_INSIDE',1);
+define('WEB_ROOT',dirname($_SERVER["SCRIPT_FILENAME"]));
+define('SMARTY_DIR',WEB_ROOT.'/lib/smarty/libs/');
+
+define('TEMPLATE_DIR',WEB_ROOT . '/templates/');
+if( is_dir(WEB_ROOT . '/templates/' . $_SERVER['SERVER_NAME']) ) {
+  define('TEMPLATE_STYLE',$_SERVER['SERVER_NAME']);
+} else {
+  define('TEMPLATE_STYLE','.');
+}
 
 require_once('inc.function.php');
 require_once('lib/inc.database.php');
