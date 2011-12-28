084edbf88ceb54bbc45901c07e7c1e3113078233
Frank Agerholm <franka@serverwg.de>
2011-12-28 11:33:27 +0100

    Added Plugin for Login/Logout

---
+++ web/lib/class.Plugin_Login.php
@@ -0,0 +1,101 @@
+<?php
+
+require_once('lib/func.http_get_var.php');
+
+class Plugin_Login {
+
+	private $pdo = null;
+
+	function __construct($pdo) {
+		$this->pdo = $pdo;
+
+		// AutoLogout
+		if(isset($_SESSION['_login_ok']) && $_SESSION['_login_ok'] == 1) {
+			$logout = http_get_var('logout');
+			if($logout) {
+				unset($_SESSION['_login_ok']);
+			}
+		}
+	}
+	
+ protected  function loglogin($accountid) {
+		$SQL = "INSERT INTO logins (accountid,logintime) VALUES (".$accountid.",NOW())";
+		try {
+			$st = $this->pdo->prepare($SQL);
+			$st->execute(array($accountid));
+		} catch (PDOException $e) {
+			print $e;
+		}
+	}
+
+	public function checkAuth() {
+		global $_SESSION;
+		$ret = FALSE;
+		$auth_user = http_get_var('auth_user');
+		$auth_pass = http_get_var('auth_pass');
+		if($auth_user != '' && $auth_pass != '') {
+			// Formular abgeschickt
+			try {
+				$SQL = "SELECT accountid,username,acl FROM account WHERE username=? AND passwd=MD5(?) AND active=1";
+				$st = $this->pdo->prepare($SQL);
+				$st->execute(array($auth_user,$auth_pass));
+				if($row = $st->fetch(PDO::FETCH_ASSOC)) {
+						if($row) {
+							$_SESSION['_login_ok'] = 1;
+							$_SESSION['_accountid'] = $row['accountid'];
+							$_SESSION['_username'] = $row['username'];
+							$_SESSION['_acl'] = $row['acl'];
+							$this->loglogin($row['accountid']);
+							$ret = TRUE;
+						}
+				}
+				$st->closeCursor();
+			}catch (PDOException $e) {
+				print $e;
+			}
+		}
+		$auth_user = '';
+		$auth_pass = '';
+		return $ret;
+	}
+
+	public function checkNewPw() {
+		$newpw = http_get_var('newpw',0);
+		$user = http_get_var('user');
+		$email = http_get_var('email');
+		
+		if( $newpw == 1 ) {
+			// show form
+			// TODO
+		}
+	}
+
+	public function getContent() {
+		
+	}
+
+	protected function new_password($user, $email)
+	{
+		$content = "qwertzupasdfghkyxcvbnm";
+		$content .= "123456789";
+		srand((double)microtime()*1000000);
+		$rand_password = '';
+		for($i = 0; $i < 8; $i++)
+		{
+			$rand_password .= substr($content,(rand()%(strlen ($content))), 1);
+		}
+		try {
+			$SQL1 = "UPDATE account SET passwd = MD5(?) WHERE username = ? AND email = ?";
+			$st = $this->pdo->prepare($SQL);
+			$st->execute(array($rand_password,$user,$email));
+			if($st->rowCount() != 1) {
+				$rand_password = FALSE;
+			}
+		} catch ( PDOException $e) {
+			print $e;
+		}
+		return $rand_password;
+	}
+}
+
+?>
