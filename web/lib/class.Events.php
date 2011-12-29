0007ac592c327c5e7bb6d15697e10c35883764e9
Frank Agerholm <franka@serverwg.de>
2011-12-29 20:44:00 +0100

    New method for querying all events for a domain

--- web/lib/class.Events.php
+++ web/lib/class.Events.php
@@ -12,14 +12,36 @@ class Events  {
 	}
 
 	/**
-	 * Get list of Events for specified domain
+	 * Get list of Events for specified domain without child-events
 	 */
 	public function getRootEvents($domainid) {
 		$ret = array();
 		try {
 			$SQL = 'SELECT e.*,de.domainid FROM event_event e 
 			LEFT JOIN domain_event de ON e.eventid=de.eventid
-			WHERE e.hidden=0 AND e.parent IS NULL AND de.domainid=?';
+			WHERE e.hidden=0 AND e.parent IS NULL AND de.domainid=? ORDER BY e.sort';
+			$st = $this->pdo->prepare($SQL);
+			$st->execute(array($domainid));
+			while( $row = $st->fetch(PDO::FETCH_ASSOC) ) {
+				$ret[] = $row;
+			}
+			$st->closeCursor();
+		} catch (PDOException $e) {
+			print $e;
+		}
+		print_r($ret);
+		return $ret;
+	}
+
+	/**
+	 * Get list of all Events for specified domain
+	 */
+	public function getEvents($domainid) {
+		$ret = array();
+		try {
+			$SQL = 'SELECT e.*,de.domainid FROM event_event e 
+			LEFT JOIN domain_event de ON e.eventid=de.eventid
+			WHERE e.hidden=0 AND de.domainid=? ORDER BY e.sort';
 			$st = $this->pdo->prepare($SQL);
 			$st->execute(array($domainid));
 			while( $row = $st->fetch(PDO::FETCH_ASSOC) ) {
@@ -31,7 +53,6 @@ class Events  {
 		}
 		return $ret;
 	}
-
 }
 
 ?>
