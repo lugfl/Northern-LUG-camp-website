fd461c7afde546a3876b7056dcb18941d6fac793
Frank Agerholm <frank@lugfl.de>
2016-12-08 19:38:31 +0100

    added composer autoloader to global.php

--- web/global.php
+++ web/global.php
@@ -4,7 +4,6 @@ setlocale (LC_ALL, 'de_DE');
 
 define('WEB_INSIDE',1);
 define('WEB_ROOT',dirname($_SERVER["SCRIPT_FILENAME"]));
-define('SMARTY_DIR',WEB_ROOT.'/lib/smarty/libs/');
 define('XINHA_WEBROOT', '/lib/xinha/');
 
 define('TEMPLATE_DIR',WEB_ROOT . '/templates/');
@@ -14,7 +13,9 @@ if( is_dir(WEB_ROOT . '/templates/' . $_SERVER['SERVER_NAME']) ) {
   define('TEMPLATE_STYLE','.');
 }
 
-//require_once('lib/inc.database.php');
+// load composer autoloader
+require_once('../vendor/autoload.php');
+
 require_once('config.php');
 
 require_once('lib/inc.sponsoren.php');
